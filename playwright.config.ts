bd84063c85c2f8ada856337aca0ba381dea0e71a
Jose Daniel Gastelu <danielgastelu.s@gmail.com>
2026-07-10 21:04:32 -0500

    feat: update headless: true

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
         trace: 'retain-on-failure',
         screenshot: 'only-on-failure',
         video: 'on-first-retry',
-        headless: false,
+        headless: true,
     },
 
     projects: [
